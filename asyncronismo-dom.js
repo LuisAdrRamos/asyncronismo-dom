8ec5ad57b7f6a40f62ba0a11940c0faec338299a
Luis Adrian Ramos <150483323+keepchup@users.noreply.github.com>
2024-10-18 20:18:39 -0500

    API use

--- asyncronismo-dom.js
+++ asyncronismo-dom.js
@@ -1,1 +1,25 @@
+// Uso de API's
 
+// link del github de la API https://github.com/shevabam/breaking-bad-quotes.git
+// API en cuestion https://api.breakingbadquotes.xyz
+
+// then y catch
+fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+    .then((request) => (request.json()))
+    .then((data) => {console.log('Frase 1 de Breaking Bad:\n',data,'\n')})
+
+    .catch((error) => {console.log(error)})
+
+
+// async y await
+const getBreakingBadAPI = async () => {
+    try {
+        let request = await fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+        let data = await request.json()
+        console.log('Frase 2 de Breaking Bad:\n',data,'\n');
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+getBreakingBadAPI()
